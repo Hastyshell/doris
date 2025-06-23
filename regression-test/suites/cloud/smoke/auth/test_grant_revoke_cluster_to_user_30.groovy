78af29660e92d586bc27bda20eb86283c67a6f06
chunping <zhangchunping@selectdb.com>
2025-06-23 21:08:40 +0800

    [test][imporvement] resolve smoke case IllegalArgumentException (#4117)

--- regression-test/suites/cloud/smoke/auth/test_grant_revoke_cluster_to_user_30.groovy
+++ regression-test/suites/cloud/smoke/auth/test_grant_revoke_cluster_to_user_30.groovy
@@ -52,7 +52,7 @@ suite("smoke_test_grant_revoke_cluster_to_user_30", "smoke") {
     // ${user1} admin role
     sql """create user ${user1} identified by 'Cloud12345' default role 'admin'"""
     sql "sync"
-    result = sql_return_maparray """show grants for '${user1}'"""
+    def result = sql_return_maparray """show grants for '${user1}'"""
     commonAuth result, "'${user1}'@'%'" as String, "Yes", "admin", "Admin_priv"
     assertNull(result.CloudClusterPrivs[0])
 
