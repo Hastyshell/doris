811268f37378a2f4653bb2aa970c0313690a89b5
Luwei <luwei@selectdb.com>
2025-08-20 15:10:28 +0800

    add tde enterprise code

---
+++ be/src/enterprise/encrypted_file_system.cpp
@@ -0,0 +1,231 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+#include "enterprise/encrypted_file_system.h"
+
+#include <gen_cpp/olap_file.pb.h>
+#include <glog/logging.h>
+#include <s2/base/integral_types.h>
+
+#include <cstddef>
+#include <cstdint>
+#include <memory>
+
+#include "common/status.h"
+#include "enterprise/encrypted_file_reader.h"
+#include "enterprise/encrypted_file_writer.h"
+#include "enterprise/encryption_common.h"
+#include "io/fs/file_reader.h"
+#include "io/fs/file_reader_writer_fwd.h"
+#include "runtime/workload_management/io_context.h"
+#include "util/coding.h"
+#include "util/encryption_util.h"
+
+namespace doris::io {
+
+// info_pb_len(uint32_t) + version(uint8_t) + magic_code(uint64_t)
+constexpr uint64_t footer_info_len = sizeof(uint32_t) + sizeof(uint8_t) + sizeof(uint64_t);
+
+Status EncryptedFileSystem::create_file_impl(const Path& file, FileWriterPtr* writer,
+                                             const FileWriterOptions* opts) {
+    auto maybe_encryption_info = io::EncryptionInfo::create(_algorithm);
+    if (!maybe_encryption_info) {
+        return Status::InternalError("create encryption info error: {}",
+                                     maybe_encryption_info.error());
+    }
+    auto encryption_info = std::move(maybe_encryption_info.value());
+
+    switch (encryption_info->data_key->algorithm()) {
+    case EncryptionAlgorithmPB::AES_256_CTR:
+    case EncryptionAlgorithmPB::SM4_128_CTR:
+        break;
+    default:
+        return Status::InvalidArgument(
+                "Invalid encryption mode {}, only support AES_256_CTR or SM4_128_CTR for now",
+                encryption_info->data_key->algorithm());
+    }
+
+    RETURN_IF_ERROR(_fs_inner->create_file(file, writer, opts));
+    *writer = std::make_unique<EncryptedFileWriter>(std::move(*writer), std::move(encryption_info));
+    return Status::OK();
+}
+
+Result<FileEncryptionInfoPB> parse_footer(const Slice footer) {
+    Slice magic_code_slice(footer.data + (footer.size - sizeof(uint64)), sizeof(uint64_t));
+    auto magic_code = decode_fixed64_le(reinterpret_cast<uint8_t*>(magic_code_slice.data));
+    if (magic_code != MAGIC_CODE) {
+        return ResultError(
+                Status::Corruption("Wrong magic code: {}, not a doris encrypted file", magic_code));
+    }
+
+    // ignore version field
+    Slice info_len_slice(magic_code_slice.data - sizeof(uint8_t) - sizeof(uint32_t),
+                         sizeof(uint32_t));
+    auto info_pb_len = decode_fixed32_le(reinterpret_cast<uint8_t*>(info_len_slice.data));
+    Slice info_pb_slice(footer.data, info_pb_len);
+    FileEncryptionInfoPB info_pb;
+    if (!info_pb.ParseFromArray(info_pb_slice.data, info_pb_slice.size)) {
+        return ResultError(Status::Corruption("parse encryption info failed"));
+    }
+    return info_pb;
+}
+
+Status open_file_with_file_size(FileSystem* fs_inner, const Path& file, FileReaderSPtr* reader,
+                                const FileReaderOptions* opts) {
+    DCHECK_NE(opts, nullptr);
+    DCHECK_NE(opts->file_size, -1);
+
+    FileReaderOptions tmp_opts = *opts;
+    tmp_opts.file_size = opts->file_size + sizeof(uint64_t);
+    FileReaderSPtr tmp_reader;
+    RETURN_IF_ERROR(fs_inner->open_file(file, &tmp_reader, &tmp_opts));
+    Defer defer {[&tmp_reader]() {
+        auto st = tmp_reader->close();
+        LOG(WARNING) << "Close tmp reader failure=" << st;
+    }};
+
+    uint8_t footer_len_buf[sizeof(uint64_t)];
+    Slice footer_len_slice(footer_len_buf, sizeof(uint64_t));
+    size_t bytes_read;
+    IOContext dummy_io_cyx;
+    RETURN_IF_ERROR(
+            tmp_reader->read_at(opts->file_size, footer_len_slice, &bytes_read, &dummy_io_cyx));
+    if (bytes_read < sizeof(uint64_t)) {
+        return Status::Corruption("Insufficient bytes to read footer length field, bytes_read={}",
+                                  bytes_read);
+    }
+
+    auto footer_len = decode_fixed64_le(footer_len_buf);
+    if (footer_len < footer_info_len) {
+        return Status::Corruption("Insufficient bytes to reader footer, footer_len={}", footer_len);
+    }
+
+    tmp_opts.file_size += footer_len;
+    RETURN_IF_ERROR(fs_inner->open_file(file, reader, &tmp_opts));
+
+    auto reader_inner = *reader;
+    std::vector<uint8_t> footer_buf(footer_len);
+    Slice footer(footer_buf.data(), footer_len);
+    RETURN_IF_ERROR(reader_inner->read_at(tmp_opts.file_size - footer_len, footer, &bytes_read,
+                                          &dummy_io_cyx));
+    if (bytes_read != footer_len) {
+        return Status::Corruption("Insufficient bytes for footer, bytes_read={}, footer_len={}",
+                                  bytes_read, footer_len);
+    }
+
+    auto info_pb = DORIS_TRY(parse_footer(footer));
+    auto encryption_info = DORIS_TRY(EncryptionInfo::load(info_pb));
+    *reader = std::make_shared<EncryptedFileReader>(std::move(reader_inner),
+                                                    std::move(encryption_info), opts->file_size);
+
+    return Status::OK();
+}
+
+Status open_file_normal(FileSystem* fs_inner, const Path& file, FileReaderSPtr* reader,
+                        const FileReaderOptions* opts) {
+    RETURN_IF_ERROR(fs_inner->open_file(file, reader, opts));
+    IOContext dummy_io_cyx;
+    auto reader_inner = *reader;
+    auto file_size = reader_inner->size();
+    std::vector<uint8_t> footer_info_buf(footer_info_len);
+    Slice footer_info_slice(footer_info_buf.data(), footer_info_len);
+    size_t bytes_read;
+    RETURN_IF_ERROR(reader_inner->read_at(file_size - footer_info_len, footer_info_slice,
+                                          &bytes_read, &dummy_io_cyx));
+    if (bytes_read != footer_info_len) {
+        return Status::Corruption(
+                "Insufficient bytes to parse magic code, version and pb length, bytes_read={}, "
+                "expect={}",
+                bytes_read, footer_info_len);
+    }
+    Slice magic_code_slice(footer_info_slice.data + sizeof(uint32) + sizeof(uint8_t),
+                           sizeof(uint64_t));
+    auto magic_code = decode_fixed64_le(reinterpret_cast<uint8_t*>(magic_code_slice.data));
+    if (magic_code != MAGIC_CODE) {
+        return Status::Corruption("Wrong magic code: {}, not a doris encrypted file", magic_code);
+    }
+    Slice info_len_slice(footer_info_slice.data, sizeof(uint32_t));
+    auto info_pb_len = decode_fixed32_le(reinterpret_cast<uint8_t*>(info_len_slice.data));
+
+    std::vector<uint8_t> info_pb_buf(info_pb_len);
+    Slice info_pb_slice(info_pb_buf.data(), info_pb_len);
+    RETURN_IF_ERROR(reader_inner->read_at(file_size - footer_info_len - info_pb_len, info_pb_slice,
+                                          &bytes_read, &dummy_io_cyx));
+    if (bytes_read != info_pb_len) {
+        return Status::Corruption("Insufficient bytes for info pb, bytes_read={}, expect={}",
+                                  bytes_read, info_pb_len);
+    }
+    FileEncryptionInfoPB info_pb;
+    if (!info_pb.ParseFromArray(info_pb_slice.data, info_pb_slice.size)) {
+        return Status::Corruption("parse encryption info failed");
+    }
+
+    auto encryption_info = DORIS_TRY(EncryptionInfo::load(info_pb));
+    *reader = std::make_shared<EncryptedFileReader>(
+            std::move(reader_inner), std::move(encryption_info),
+            file_size - (sizeof(uint64_t) + info_pb_len + sizeof(uint32_t) + sizeof(uint8_t) +
+                         sizeof(uint64_t)));
+    return Status::OK();
+}
+
+Status EncryptedFileSystem::open_file_impl(const Path& file, FileReaderSPtr* reader,
+                                           const FileReaderOptions* opts) {
+    if (opts != nullptr && opts->file_size != -1) {
+        return open_file_with_file_size(_fs_inner.get(), file, reader, opts);
+    }
+    return open_file_normal(_fs_inner.get(), file, reader, opts);
+}
+
+Status EncryptedFileSystem::create_directory_impl(const Path& dir, bool failed_if_exists) {
+    return _fs_inner->create_directory(dir, failed_if_exists);
+}
+
+Status EncryptedFileSystem::delete_file_impl(const Path& file) {
+    return _fs_inner->delete_file(file);
+}
+
+Status EncryptedFileSystem::delete_directory_impl(const Path& dir) {
+    return _fs_inner->delete_directory(dir);
+}
+
+Status EncryptedFileSystem::batch_delete_impl(const std::vector<Path>& files) {
+    return _fs_inner->batch_delete(files);
+}
+
+Status EncryptedFileSystem::exists_impl(const Path& path, bool* res) const {
+    return _fs_inner->exists(path, res);
+}
+
+Status EncryptedFileSystem::file_size_impl(const Path& file, int64_t* file_size) const {
+    return _fs_inner->file_size(file, file_size);
+}
+
+Status EncryptedFileSystem::list_impl(const Path& dir, bool only_file, std::vector<FileInfo>* files,
+                                      bool* exists) {
+    return _fs_inner->list(dir, only_file, files, exists);
+}
+
+Status EncryptedFileSystem::rename_impl(const Path& orig_name, const Path& new_name) {
+    return _fs_inner->rename(orig_name, new_name);
+}
+
+Status EncryptedFileSystem::absolute_path(const Path& path, Path& abs_path) const {
+    abs_path = path;
+    return Status::OK();
+}
+
+} // namespace doris::io
---
+++ fe/fe-enterprise/src/main/java/org/apache/doris/enterprise/KeyManager.java
@@ -0,0 +1,192 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package org.apache.doris.enterprise;
+
+import org.apache.doris.catalog.Env;
+import org.apache.doris.common.Config;
+import org.apache.doris.encryption.DataKeyMaterial;
+import org.apache.doris.encryption.EncryptionKey;
+import org.apache.doris.encryption.KeyManagerInterface;
+import org.apache.doris.encryption.KeyManagerStore;
+import org.apache.doris.encryption.RootKeyInfo;
+import org.apache.doris.persist.KeyOperationInfo;
+
+import com.google.common.base.Preconditions;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.Base64;
+import java.util.List;
+import java.util.zip.CRC32;
+
+public class KeyManager implements KeyManagerInterface {
+    private static final int AES256_KEY_LENGTH = 32;
+
+    private static final int SM4_KEY_LENGTH = 16;
+
+    private static final Logger LOG = LogManager.getLogger(KeyManager.class);
+
+    private RootKeyProvider rootKeyProvider;
+
+    private KeyManagerStore store;
+
+    //private TreeMap<Integer, EncryptionKey> aes256MasterKeys = new TreeMap<>();
+
+    //private TreeMap<Integer, EncryptionKey> sm4MasterKeys = new TreeMap<>();
+
+    public void setRootKey(RootKeyInfo rootKeyInfo) throws RuntimeException {
+        if (rootKeyInfo.type == RootKeyInfo.RootKeyType.AWS_KMS) {
+            rootKeyProvider = new AwsKmsRootKeyProvider();
+        } else if (rootKeyInfo.type == RootKeyInfo.RootKeyType.LOCAL) {
+            rootKeyProvider = new LocalRootKeyProvider();
+        } else {
+            throw new RuntimeException("not support root key type" + rootKeyInfo.type);
+        }
+
+        try {
+            rootKeyProvider.init(rootKeyInfo);
+            rootKeyProvider.describeKey();
+        } catch (Exception e) {
+            throw new RuntimeException("failed to set root key", e);
+        }
+
+        //TODO(luwei) lock
+        KeyOperationInfo opInfo = new KeyOperationInfo();
+        opInfo.setRootKeyInfo(rootKeyInfo);
+        EncryptionKey aes256MasterKey = generateMasterKey(EncryptionKey.Algorithm.AES256, rootKeyInfo);
+        opInfo.addMasterKey(aes256MasterKey);
+        EncryptionKey sm4MasterKey = generateMasterKey(EncryptionKey.Algorithm.SM4, rootKeyInfo);
+        opInfo.addMasterKey(sm4MasterKey);
+        opInfo.setOpType(KeyOperationInfo.KeyOPType.SET_ROOT_KEY);
+        // write edit log
+        Env.getCurrentEnv().getEditLog().logOperateKey(opInfo);
+
+        store = Env.getCurrentEnv().getKeyManagerStore();
+        store.addMasterKey(sm4MasterKey);
+        store.addMasterKey(aes256MasterKey);
+        store.setRootKeyInfo(rootKeyInfo);
+
+        //aes256MasterKeys.put(aes256MasterKey.version, aes256MasterKey):
+        //sm4MasterKeys.put(sm4MasterKey.version, sm4MasterKey);
+    }
+
+    public void setRootKeyByConfig() {
+        if (Config.doris_tde_key_id.isEmpty() || Config.doris_tde_key_endpoint.isEmpty()
+                || Config.doris_tde_key_provider.isEmpty() || Config.doris_tde_key_region.isEmpty()) {
+            return;
+        }
+
+        RootKeyInfo rootKeyInfo = new RootKeyInfo();
+        // TODO(luwei) check field
+        rootKeyInfo.type = RootKeyInfo.RootKeyType.valueOf(Config.doris_tde_key_provider.toUpperCase());
+        rootKeyInfo.cmkId = Config.doris_tde_key_id;
+        rootKeyInfo.region = Config.doris_tde_key_region;
+        rootKeyInfo.endpoint = Config.doris_tde_key_endpoint;
+
+        setRootKey(rootKeyInfo);
+    }
+
+    public void setLocalRootKey() {
+        String password = System.getenv("DORIS_TDE_ROOT_KEY_PASSWORD");
+
+        RootKeyInfo rootKeyInfo = new RootKeyInfo();
+        rootKeyInfo.type = RootKeyInfo.RootKeyType.LOCAL;
+        rootKeyInfo.password = password;
+
+        setRootKey(rootKeyInfo);
+    }
+
+    public void init() {
+        store = Env.getCurrentEnv().getKeyManagerStore();
+        RootKeyInfo rootKeyInfo = store.getRootKeyInfo();
+        if (rootKeyInfo == null) {
+            setRootKeyByConfig();
+            return;
+        }
+
+        if (rootKeyInfo.type == RootKeyInfo.RootKeyType.AWS_KMS) {
+            rootKeyProvider = new AwsKmsRootKeyProvider();
+        } else {
+            throw new RuntimeException("not support root key type" + rootKeyInfo.type);
+        }
+
+        try {
+            rootKeyProvider.init(rootKeyInfo);
+            rootKeyProvider.describeKey();
+            // TODO(luwei) check permissions
+        } catch (Exception e) {
+            throw new RuntimeException("failed to set root key", e);
+        }
+
+        decryptMasterKey();
+    }
+
+    @Override
+    public void replayKeyOperation(KeyOperationInfo keyOpInfo) {
+        store = Env.getCurrentEnv().getKeyManagerStore();
+        store.setRootKeyInfo(keyOpInfo.getRootKeyInfo());
+        for (EncryptionKey key : keyOpInfo.getMasterKeys()) {
+            store.addMasterKey(key);
+        }
+    }
+
+    public long computeCrc(byte[] plaintext) {
+        CRC32 crc32 = new CRC32();
+        crc32.update(plaintext);
+        return crc32.getValue();
+    }
+
+    private EncryptionKey generateMasterKey(EncryptionKey.Algorithm algorithm, RootKeyInfo rootKeyInfo) {
+        int keyLength = -1;
+        if (algorithm == EncryptionKey.Algorithm.AES256) {
+            keyLength = AES256_KEY_LENGTH;
+        } else if (algorithm == EncryptionKey.Algorithm.SM4) {
+            keyLength = SM4_KEY_LENGTH;
+        } else {
+            throw new RuntimeException("invalid encryption algorithm: " + algorithm.toString());
+        }
+
+        DataKeyMaterial material = rootKeyProvider.generateSymmetricDataKey(keyLength);
+        EncryptionKey masterKey = new EncryptionKey();
+        masterKey.id = Long.toString(Env.getCurrentEnv().getNextId());
+        masterKey.version = 1;
+        masterKey.parentId = rootKeyInfo.cmkId;
+        masterKey.parentVersion = 1;
+        masterKey.algorithm = EncryptionKey.Algorithm.AES256;
+        masterKey.type = EncryptionKey.KeyType.MASTER_KEY;
+        masterKey.plaintext = material.plaintext;
+        masterKey.ciphertext = Base64.getEncoder().encodeToString(material.ciphertext);
+        masterKey.ctime = System.currentTimeMillis();
+        masterKey.mtime = System.currentTimeMillis();
+        masterKey.crc = computeCrc(masterKey.plaintext);
+        return masterKey;
+    }
+
+    public List<EncryptionKey> getAllMasterKeys() {
+        return store.getMasterKeys();
+    }
+
+    private void decryptMasterKey() {
+        List<EncryptionKey> masterKey = store.getMasterKeys();
+        for (EncryptionKey versionKey : masterKey) {
+            Preconditions.checkArgument(versionKey.plaintext == null || versionKey.plaintext.length == 0);
+            versionKey.plaintext = rootKeyProvider.decrypt(Base64.getDecoder().decode(versionKey.ciphertext));
+            Preconditions.checkArgument(versionKey.crc == computeCrc(versionKey.plaintext));
+        }
+    }
+}
