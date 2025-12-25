b630afdd01a208380ef36a5d42dceb8f351d2c03
Siyang Tang <tangsiyang@selectdb.com>
2025-12-25 11:13:30 +0800

    [fix](delete) Fix delete stuck for NDSD (#409)

--- fe/fe-core/src/main/java/org/apache/doris/load/DeleteJob.java
+++ fe/fe-core/src/main/java/org/apache/doris/load/DeleteJob.java
@@ -408,19 +408,6 @@ public class DeleteJob extends AbstractTxnStateChangeCallback implements DeleteJ
                 throw new UserException(String.format("delete job timeout, timeout(ms):%s, msg:%s", timeoutMs, errMsg));
             case QUORUM_FINISHED:
             case FINISHED:
-                long nowQuorumTimeMs = System.currentTimeMillis();
-                long endQuorumTimeoutMs = nowQuorumTimeMs + timeoutMs / 2;
-                // if job's state is quorum_finished then wait for a period of time and commit it.
-                while (state == DeleteState.QUORUM_FINISHED
-                        && endQuorumTimeoutMs > nowQuorumTimeMs) {
-                    checkAndUpdateQuorum();
-                    Thread.sleep(1000);
-                    nowQuorumTimeMs = System.currentTimeMillis();
-                    if (LOG.isDebugEnabled()) {
-                        LOG.debug("wait for quorum finished delete job: {}, txn id: {}",
-                                id, signature);
-                    }
-                }
                 break;
             default:
                 throw new IllegalStateException("wrong delete job state: " + state.name());
